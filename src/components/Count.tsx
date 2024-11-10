d6c4d8db523e1cb891f9773fc826d80512161355
fumio <p30f22g50@gmail.com>
2024-11-10 19:00:31 +0900

    Add Count

---
+++ src/components/Count.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { useState } from "react";
+
+function Count() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="m-2 flex flex-col items-center justify-center gap-4">
+      <p className="text-4xl">{count}</p>
+      <button
+        className="text-3xl font-bold"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        Click me!
+      </button>
+    </div>
+  );
+}
+
+export default Count;

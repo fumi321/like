03efa70991df4c0221f80d1ed153046fb5a23798
fumio <p30f22g50@gmail.com>
2024-11-12 13:40:11 +0900

    Don't use api

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -1,32 +1,23 @@
 "use client";
 
+import { getLikeCount, incrementLike } from "@/server/actions";
 import React, { useEffect, useState } from "react";
 
 export default function Count() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    fetch("/api/count")
-      .then((res) => res.json())
-      .then((data) => setCount(data.like))
-      .catch(() => console.error("Failed to fetch count"));
+    getLikeCount().then(setCount);
   }, []);
-
-  const handleClick = async () => {
-    fetch("/api/count", {
-      method: "POST",
-    })
-      .then((res) => res.json())
-      .then((data) => setCount(data.like))
-      .catch(() => console.error("Failed to update count"));
-  };
 
   return (
     <div className="m-2 flex flex-col items-center justify-center gap-4">
       <p className="text-4xl">{count}</p>
       <button
         className="rounded bg-blue-500 px-4 py-2 text-3xl font-bold text-white hover:bg-blue-600 disabled:bg-gray-400"
-        onClick={handleClick}
+        onClick={() => {
+          incrementLike().then(setCount);
+        }}
       >
         Click me!
       </button>
---
+++ src/server/actions.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import prisma from "@/db/prisma";
+
+export async function getLikeCount() {
+  try {
+    const user =
+      (await prisma.user.findFirst()) ||
+      (await prisma.user.create({ data: { like: 0 } }));
+    return user.like;
+  } catch {
+    return 0;
+  }
+}
+
+export async function incrementLike() {
+  try {
+    let user = await prisma.user.findFirst();
+    user = await prisma.user.update({
+      where: { id: user?.id },
+      data: {
+        like: { increment: 1 },
+      },
+    });
+    return user.like;
+  } catch {
+    return getLikeCount();
+  }
+}
